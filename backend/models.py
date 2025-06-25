424b2a7bada79cea2830590f25e3b3299576b4ca
David Fine <davidfine@Davids-MacBook-Pro.local>
2025-06-25 12:50:57 -0400

    add __str__ to Song

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Optional
 
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -16,6 +17,19 @@ class Song(models.Model):
     lyricist = models.CharField(max_length=100, default=None, null=True)
     show = models.CharField(max_length=60, default=None, null=True)
     year = models.IntegerField(validators=[MinValueValidator(1000), MaxValueValidator(current_year)])
+
+    def __str__(self):
+        return " - ".join(
+            [item for item in [self.title, self.show, self.album] if item]
+        )
+
+    @property
+    def decade(self) -> Optional[str]:
+        if not self.year:
+            return None
+        decade_value = (self.year // 10) * 10
+        return f"{decade_value}s"
+
 
 class Arrangement(models.Model):
     name = models.CharField(max_length=100)
