493bf9cc142bc2dc76d1e8b8b8d8a2190181321a
David Fine <davidfine@Davids-MacBook-Pro.local>
2025-06-25 13:36:43 -0400

    list songs on index

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from backend.models import Song
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the backend index.")
+    song_list = Song.objects.order_by("title")
+    template = loader.get_template("index.html")
+    context = {"songs": song_list}
+    return HttpResponse(template.render(context, request))
+
+
+def song(request, song_title: str):
+    return HttpResponse(f"This is the page giving details for {song_title}.")
